d2ec796dcc14a16b03e602ef604b94fa925e52e5
Nikola Milojević <nikola@milojevic.me>
2026-02-04 10:27:40 +0100

    sign mac libraries with quill

---
+++ scripts/sign-macos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+OS="$1"
+BINARY="$2"
+IS_SNAPSHOT="$3"
+
+if [ "$OS" != "darwin" ]; then
+  echo "skipping signing for $OS"
+  exit 0
+fi
+
+quill sign-and-notarize "$BINARY" --dry-run="$IS_SNAPSHOT" --ad-hoc="$IS_SNAPSHOT" -vv
